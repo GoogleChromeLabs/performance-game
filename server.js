c0b52f8acde9d24d11eb977fd4bf932adff3e07d
martinschierle <martin.schierle@gmail.com>
2018-08-03 23:39:15 +0200

    Removed unused imports

--- server.js
+++ server.js
@@ -16,7 +16,6 @@ limitations under the License.
 const fs = require('fs')
 const path = require('path')
 const bodyParser = require('body-parser')
-const parseCanonicalUrl = require('parse-canonical-url');
 const low = require('lowdb')
 const FileSync = require('lowdb/adapters/FileSync')
 const adapter = new FileSync('.data/db.json')
