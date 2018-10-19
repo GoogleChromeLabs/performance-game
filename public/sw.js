f1ddf87db63189c33480c0348007d091c8a98ce3
martinschierle <martin.schierle@gmail.com>
2018-10-19 10:50:28 +0200

    Added in full config for lighthouse, to get the unused-javascript audit, to also color js asteroids. Also fixed small sw bug.

--- public/sw.js
+++ public/sw.js
@@ -5,6 +5,11 @@ console.log('this is my custom service worker');
 workbox.precaching.precacheAndRoute([]);
 
 workbox.routing.registerRoute(
+  new RegExp('.*gamestate.json.*'),
+  workbox.strategies.networkFirst()
+);
+
+workbox.routing.registerRoute(
   new RegExp('.*'),
   workbox.strategies.networkFirst({"networkTimeoutSeconds": 1})
 );
