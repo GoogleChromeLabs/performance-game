c516ba974e482996785a87372e15a574cb27949f
martinschierle <martin.schierle@gmail.com>
2018-12-19 16:10:47 +0100

    Fixed some minor bugs, removed endscreen, instead route user through to new PSI website for the current URL to analyze

--- public/sw.js
+++ public/sw.js
@@ -1,7 +1,5 @@
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/3.0.0/workbox-sw.js');
 
-console.log('this is my custom service worker');
-
 workbox.precaching.precacheAndRoute([]);
 
 workbox.routing.registerRoute(
